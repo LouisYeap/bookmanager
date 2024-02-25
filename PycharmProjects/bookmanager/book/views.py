36069642d693dfa29995eae28f8a716dac517596
louis <18758398381@163.com>
2024-02-25 22:15:07 +0800

    book

--- PycharmProjects/bookmanager/book/views.py
+++ PycharmProjects/bookmanager/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 视图函数第一个参数就是接收请求，
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('ok')
