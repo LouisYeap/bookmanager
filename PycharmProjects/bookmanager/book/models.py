1554f82b682455ef0e3041f2f5e35fd81d673efc
louis <18758398381@163.com>
2024-02-28 23:02:37 +0800

    book

--- PycharmProjects/bookmanager/book/models.py
+++ PycharmProjects/bookmanager/book/models.py
@@ -4,7 +4,16 @@ from django.db import models
 # Create your models here.
 class BookInfo(models.Model):
     # django自动提供了主键id
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, unique=True)
+    pub_date = models.DateField(null=True)
+    read_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    # 在Django框架中，class Meta 是一个内部类，用于定义模型的一些元数据。这些元数据不会成为数据库表的一部分，但它们为Django的ORM（对象关系映射）层提供了关于如何与数据库交互的额外信息。
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
 
     # 重写str方法显示书籍名字
     def __str__(self):
@@ -14,6 +23,16 @@ class BookInfo(models.Model):
 # 人物
 
 class PeopleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    gender = models.BooleanField()
+    GENDER_CHOICE = (
+        (1, '男'),
+        (2, '女')
+    )
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=10, null=True)
+    is_delete = models.BooleanField(default=False)
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+
--- PycharmProjects/bookmanager/bookmanager/settings.py
+++ PycharmProjects/bookmanager/bookmanager/settings.py
@@ -72,10 +72,20 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'NAME': 'book_42_01',
     }
 }
 
